e3b76b056fbe73f6a1f740cfc43fc3096dc87ec9
zandroid <30754017+phantomzidane@users.noreply.github.com>
2019-10-29 10:02:27 +0100

    started editing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HomeComponent} from './home/home.component';
+import { BodyComponent } from './home/body/body.component';
+import { ServicesComponent } from './home/services/services.component';
+import { ChooseComponent } from './home/choose/choose.component';
+import { FeaturesComponent } from './home/features/features.component';
+import { AgentComponent } from './home/agent/agent.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BodyComponent,
+    ServicesComponent,
+    ChooseComponent,
+    FeaturesComponent,
+    AgentComponent
   ],
   imports: [
     BrowserModule,
